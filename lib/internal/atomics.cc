0ec450a767b21b9f5c917881e61f172985413365
Daniel Petti <djpetti@gmail.com>
2016-06-11 22:01:52 -0400

    Add ExchangeAdd function and tests for atomics.
    This is necessary for dealing with lock-free queues.

--- lib/internal/atomics.cc
+++ lib/internal/atomics.cc
@@ -5,13 +5,25 @@ namespace internal {
 
 bool CompareExchange(uint32_t *value, uint32_t old_val, uint32_t new_val) {
   uint8_t ret;
-  __asm__ __volatile__("lock\n"
-                       "cmpxchgl %2, %1\n"
-                       "sete %0\n"
-                       : "=q"(ret), "=m"(*value)
-                       : "r"(new_val), "m"(*value), "a"(old_val)
-                       : "memory");
+  __asm__ __volatile__(
+      "lock\n"
+      "cmpxchgl %2, %1\n"
+      "sete %0\n"
+      : "=q"(ret), "=m"(*value)
+      : "r"(new_val), "m"(*value), "a"(old_val)
+      : "memory");
   return ret;
+}
+
+uint32_t ExchangeAdd(uint32_t *dest, uint32_t source) {
+  uint32_t original;
+  __asm__ __volatile__(
+      "lock\n"
+      "xaddl %1, %2\n"
+      : "=r"(original)
+      : "r"(source), "m"(*dest)
+      : "memory");
+  return original;
 }
 
 }  // namespace internal
