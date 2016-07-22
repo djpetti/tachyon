3fd7e986a8021cc7ebaea7592a3e4d911c424b56
Daniel Petti <djpetti@gmail.com>
2016-07-21 19:41:22 -0700

    Make SPSC blocking tests work.
    The code is still messy in some places, and I haven't done
    MPSC blocking tests yet, but I though that I'd commit my progress
    anyway at this point.

--- lib/internal/atomics_test.cc
+++ lib/internal/atomics_test.cc
@@ -25,40 +25,41 @@ TEST_F(AtomicsTest, CompareExchangeTest) {
 
 // Make sure ExchangeAdd works properly.
 TEST_F(AtomicsTest, ExchangeAddTest) {
-  int32_t value = 2;
+  uint32_t value = 2;
   EXPECT_EQ(2, ExchangeAdd(&value, 1));
-  EXPECT_EQ(3, value);
+  EXPECT_EQ(3u, value);
 
   EXPECT_EQ(3, ExchangeAdd(&value, 2));
-  EXPECT_EQ(5, value);
+  EXPECT_EQ(5u, value);
 }
 
 // Make sure ExchangeAdd works with negative numbers.
 TEST_F(AtomicsTest, ExchangeSubtractTest) {
-  int32_t value = 2;
+  uint32_t value = 2;
   EXPECT_EQ(2, ExchangeAdd(&value, -1));
-  EXPECT_EQ(1, value);
+  EXPECT_EQ(1u, value);
 }
 
 // Make sure Exchange woeks as expected.
 TEST_F(AtomicsTest, ExchangeTest) {
   uint32_t value = 1;
-  Exchange(&value, 2);
+  uint32_t old_value = Exchange(&value, 2);
   EXPECT_EQ(2u, value);
+  EXPECT_EQ(1u, old_value);
 }
 
 // Make sure BitwiseAnd works properly.
 TEST_F(AtomicsTest, BitwiseAndTest) {
-  int32_t value = 0xFF;
+  uint32_t value = 0xFF;
   BitwiseAnd(&value, 0xF0);
-  EXPECT_EQ(0xF0, value);
+  EXPECT_EQ(0xF0u, value);
 }
 
 // Make sure Decrement works properly.
 TEST_F(AtomicsTest, DecrementTest) {
-  int32_t value = 2;
+  uint32_t value = 2;
   Decrement(&value);
-  EXPECT_EQ(1, value);
+  EXPECT_EQ(1u, value);
 }
 
 }  // namespace testing
