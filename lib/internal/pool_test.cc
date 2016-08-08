fe7640e834d080d3f7ad530663b50a9799754004
Daniel Petti <djpetti@gmail.com>
2016-08-07 22:03:52 -0400

    Add ability to ask for mem at a particular place.
    This is a similar idea to C++'s placement new. It's in preparation
    for implementing a global hashtable to make fetching queues
    possible, since we need the hashtable to remain at a deterministic
    location in memory.

--- lib/internal/pool_test.cc
+++ lib/internal/pool_test.cc
@@ -2,6 +2,7 @@
 
 #include "gtest/gtest.h"
 
+#include "constants.h"
 #include "pool.h"
 
 namespace gaia {
@@ -11,9 +12,7 @@ namespace testing {
 // Test fixture for testing the pool class.
 class PoolTest : public ::testing::Test {
  protected:
-  static const int kPoolSize = 1000;
-
-  PoolTest() : pool_(Pool::GetPool(kPoolSize)) {}
+  PoolTest() : pool_(Pool::GetPool()) {}
 
   virtual void SetUp() {
     // Clear the pool in between, so tests don't affect each-other.
@@ -32,9 +31,11 @@ class PoolTest : public ::testing::Test {
 // Make sure we can allocate shared memory from the pool.
 TEST_F(PoolTest, AllocationTest) {
   int *shared_int = pool_->AllocateForType<int>();
+  ASSERT_NE(nullptr, shared_int);
   *shared_int = 42;
 
   int *another_int = pool_->AllocateForType<int>();
+  ASSERT_NE(nullptr, another_int);
   *another_int = 1337;
   // Make sure it gave us different blocks.
   EXPECT_EQ(42, *shared_int);
@@ -46,8 +47,9 @@ TEST_F(PoolTest, AllocationTest) {
 
 // Make sure it behaves properly when we run out of pool memory.
 TEST_F(PoolTest, OverusedPoolTest) {
-  // We'll have ask for 8 separate blocks to use up all our memory.
-  for (int i = 0; i < 8; ++i) {
+  // We'll have ask for enough separate blocks to use up all our memory.
+  const int total_blocks = pool_->get_size() / pool_->get_block_size();
+  for (int i = 0; i < total_blocks; ++i) {
     EXPECT_NE(nullptr, pool_->AllocateForType<int>());
   }
 
@@ -67,23 +69,6 @@ TEST_F(PoolTest, FreeTest) {
   EXPECT_EQ(another_int, shared_int);
 }
 
-// Make sure that different pool instances stay in sync.
-TEST_F(PoolTest, SharedTest) {
-  // Allocate some data in our current pool.
-  int *shared_int = pool_->AllocateForType<int>();
-  *shared_int = 42;
-
-  // Create a new pool.
-  Pool new_pool(kPoolSize);
-  // Make sure it allocates a new block.
-  int *another_int = new_pool.AllocateForType<int>();
-  *another_int = 1337;
-  // It's not enough to compare pointers, because they could be mapped at
-  // different regions.
-  EXPECT_EQ(42, *shared_int);
-  EXPECT_EQ(1337, *another_int);
-}
-
 // Make sure that allocating a region spanning multiple blocks works.
 TEST_F(PoolTest, MultiBlockAllocationTest) {
   int *shared_array = pool_->AllocateForArray<int>(64);
@@ -98,6 +83,48 @@ TEST_F(PoolTest, MultiBlockAllocationTest) {
   for (int i = 0; i < 64; ++i) {
     EXPECT_EQ(i, shared_array[i]);
   }
+}
+
+// Test that AllocateAt() works.
+TEST_F(PoolTest, PlacementAllocationTest) {
+  // Allocate memory at a specific offset.
+  void *reserved = pool_->AllocateAt(42, 1000);
+  ASSERT_NE(nullptr, reserved);
+
+  // Make sure it actually put it at the right spot.
+  EXPECT_EQ(42, pool_->GetOffset(reserved));
+}
+
+// Make sure normal allocations work around placement allocations.
+TEST_F(PoolTest, NonOverlapTest) {
+  // Allocate memory at a specific offset.
+  const int offset = pool_->get_block_size() + 1;
+  uint8_t *reserved = pool_->AllocateAt(offset, 1000);
+  ASSERT_NE(nullptr, reserved);
+
+  // We should have a block before it to put something in.
+  uint8_t *before = pool_->Allocate(4);
+  ASSERT_NE(nullptr, reserved);
+  EXPECT_LT(before, reserved);
+
+  // Now anything else should come after it.
+  uint8_t *after = pool_->Allocate(4);
+  ASSERT_NE(nullptr, after);
+  EXPECT_GT(after, reserved);
+
+  // If we free our original allocated memory, it should be able to put
+  // something else in that space.
+  pool_->Free(reserved, 1000);
+  uint8_t *overlapping = pool_->Allocate(4);
+  ASSERT_NE(nullptr, overlapping);
+  // We asked for reserved to be one byte into the block, and it's going to put
+  // overlapping at the beginning of the block.
+  EXPECT_EQ(reserved - 1, overlapping);
+
+  // Now it should not give us that spot again, because someone's taken it in
+  // the meantime.
+  reserved = pool_->AllocateAt(offset, 1000);
+  EXPECT_EQ(nullptr, reserved);
 }
 
 }  // namespace testing
